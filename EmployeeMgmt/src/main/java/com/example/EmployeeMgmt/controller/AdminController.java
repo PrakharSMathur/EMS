af199c1126399ce60ec0b37cd48652d4b1607ddc
PrakharSMathur <32239055+PrakharSMathur@users.noreply.github.com>
2020-04-26 17:25:55 +0530

    Added few comments

--- EmployeeMgmt/src/main/java/com/example/EmployeeMgmt/controller/AdminController.java
+++ EmployeeMgmt/src/main/java/com/example/EmployeeMgmt/controller/AdminController.java
@@ -24,54 +24,65 @@ public class AdminController {
 @Autowired
   AdminService admin;
 
+  //GET request to get all details of all employees
   @GetMapping("/GetAllEmployees")
   public List<Employee> viewAllEmployees()
   {
     return admin.viewAllEmployees();
   }
 
+  //GET request to get Employee details by Employee ID at path /Admin/GetEmpployee/{empId} 
   @GetMapping("/GetEmployee/{empId}")
   public Optional<Employee> getEmployee(@PathVariable String empId){ return admin.getEmployee(empId);  }
 
+  //POST req to add New Employee, ID assigned automatically, rest data sent in raw-application/JSON format in body
   @PostMapping("/AddEmployee")
   public Serializable addStudentService(@RequestBody Employee employee) {
     return admin.addEmployeeService(employee);
   }
 
+  //PUT req to update Employee details if ID exists
   @PutMapping("/UpdateEmployee/{empId}")
   public String updateEmployee(@RequestBody Employee employee,@PathVariable String empId){
     return admin.updateEmployeeService(employee,empId);
   }
 
+  //DELETE req to delete one employee by empId
   @DeleteMapping("/DeleteEmployee/{empId}")
   public String deleteEmployee(@PathVariable String empId) {
     return admin.deleteEmployeeService(empId);
   }
 
+  //List all details of all HRs
   @GetMapping("/GetAllHRs")
   public List<HR> viewAllHRs()
   {
     return admin.viewAllHRs();
   }
 
+  //List details of one HR by ID
   @GetMapping("/GetHR/{hrId}")
   public Optional<HR> getHR(@PathVariable String hrId){
     return admin.getHR(hrId);
   }
 
+  //List HR details by HR name
   @GetMapping("/GetHRbyName/{name}")
   public Optional<HR> getHRbyName(@PathVariable String name) { return admin.getHRbyName(name);}
 
+  //Add new HR
   @PostMapping("/AddHR")
   public Serializable addHRService(@RequestBody HR hr) {
     return admin.addHRService(hr);
   }
 
+  //Delete HR by ID
   @DeleteMapping("/DeleteHR/{hrId}")
   public String deleteHR(@PathVariable String hrId) {
     return admin.deleteHRService(hrId);
   }
 
+  //Update HR details by ID
   @PutMapping("/UpdateHR/{hrId}")
   public String updateHR(@RequestBody HR hr,@PathVariable String hrId){
     return admin.updateHRService(hr,hrId);
